e900cfee76bdb23e2deb420b6a11c38050067270
Sanjeeban Das <iamsanjeebandas@gmail.com>
2025-07-19 18:22:17 +0530

    added swipe down effect to Image

--- components/header.jsx
+++ components/header.jsx
@@ -3,6 +3,7 @@ import Navigation from "./navigation";
 import Socials from "./socials";
 import MobileHeadline from "./MobileHeadline";
 import Image from "next/image";
+import AnimatedContent from "./AnimatedContent";
 
 const Header = (props) => {
   return (
@@ -10,13 +11,27 @@ const Header = (props) => {
       <div className="relative flex flex-col items-center lg:items-start">
         {/* Profile Image */}
         <div className="mb-6 flex justify-center lg:justify-start w-full">
-          <Image
-            src={props.data.profileImage}
-            alt="Sanjeeban's Photo"
-            width={160}
-            height={160}
-            className="rounded-full border-4 border-white shadow-md"
-          />
+          {/* Profile Image with swipe-down effect */}
+          <AnimatedContent
+            distance={150} // Swipe down by 150px
+            direction="vertical" // Vertical animation
+            reverse={true} // Normal downward motion
+            duration={1.2} // Smooth timing
+            ease="power3.out" // Playful easing
+            initialOpacity={0.5} // Start slightly faded
+            animateOpacity={true} // Fade-in while sliding
+            scale={1.00} // Slight zoom-in during entrance
+            threshold={0.3} // Triggers when 30% visible
+            delay={0.1} // Slight delay to pace the intro
+          >
+            <Image
+              src={props.data.profileImage}
+              alt="Sanjeeban's Photo"
+              width={160}
+              height={160}
+              className="rounded-full border-4 border-white shadow-md"
+            />
+          </AnimatedContent>
         </div>
 
         {/* Name and Headline */}
